24b58daec7c8a9bc364e58ebfef937e816e6863d
Juan Ramon De la Rosa Angle <j.delarosa48@gmail.com>
2023-06-09 07:19:19 +0200

    add models for habit and task tracker

--- habittracker/habits/models.py
+++ habittracker/habits/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Habit(models.Model):
+    """A model to represent a habit."""
+
+    class Period(models.IntegerChoices):
+        DAILY = 1
+        WEEKLY = 2
+        MONTHLY = 3
+
+    name = models.CharField(max_length=200)
+    periodicity = models.IntegerField(choices=Period.choices, default=Period.DAILY)
+    user = models.ForeignKey(User, related_name="habits", related_query_name="habits", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    streak = models.IntegerField(default=0)
+    longest_streak = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def to_representation(self, instance):
+        """Custom representation for the habit model."""
+        representation = super().to_representation(instance)
+        representation['periodicity'] = Habit.Period(representation['periodicity']).label
+        return representation
+
+
+class TaskTracker(models.Model):
+    """A model to track the completion of a habit on a given date."""
+    habit = models.ForeignKey(Habit, related_name="tasks", related_query_name="tasks", on_delete=models.CASCADE)
+    due_date = models.DateField()
+    was_done_on_time = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.habit.name} - {self.date}'
