c9c30454dbf9d3ce3f60db6ab90f297db31bd3f9
Juan Ramon De la Rosa <juan.delarosa@tenhil.de>
2023-06-12 17:19:00 +0200

    fix HabitSerializer fields

--- habittracker/habits/serializers.py
+++ habittracker/habits/serializers.py
@@ -5,15 +5,19 @@ from habits.models import Habit, TaskTracker
 
 
 class HabitSerializer(serializers.ModelSerializer):
+    current_period_due_date = serializers.SerializerMethodField()
+    periodicity = serializers.ChoiceField(choices=Habit.Period.choices)
     class Meta:
+
         model = Habit
-        fields = ['id','name', 'periodicity', 'created_at', 'is_active','current_period_due_date']
+        fields = ['id','name', 'periodicity', 'created_at', 'is_active', 'current_period_due_date']
 
-        current_period_due_date = serializers.SerializerMethodField()
-        peridoicity = serializers.ChoiceField(choices=Habit.Period.choices)
+    def get_current_period_due_date(self, obj):
+        if task := obj.tasks.filter(completed_at__isnull=True).first():
+            return task.due_date
+        return None
 
-        def get_current_period_due_date(self, obj):
-            return obj.tasks.filter(completed_at__isNull=True).first().due_date
+
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.periodicity = validated_data.get('periodicity', instance.periodicity)
